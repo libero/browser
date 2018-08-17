f5ccce0df90bd23a457cf5f29f99436b56dfb323
Chris Wilkinson <c.wilkinson@elifesciences.org>
2018-08-17 09:37:17 +0100

    Add basic content page

--- config/bundles.php
+++ config/bundles.php
@@ -1,5 +1,6 @@
 <?php
 
 return [
+    Libero\ContentPageBundle\ContentPageBundle::class => ['all' => true],
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
 ];
---
+++ vendor-extra/ContentPageBundle/src/DependencyInjection/ContentPageExtension.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Libero\ContentPageBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+final class ContentPageExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container) : void
+    {
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+
+        $loader->load('controller.xml');
+    }
+}
