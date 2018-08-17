1eb4b38020cffe959bf0c7b3ae5a6cadb6260ec5
Giannis Kopanas <jkopanas@gmail.com>
2018-08-17 14:54:48 +0300

    create api client

---
+++ vendor-extra/ApiClientBundle/src/DependencyInjection/ContentPageExtension.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Libero\ContentPageBundle\DependencyInjection;
+
+use Libero\ContentPageBundle\Controller\ContentController;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+final class ContentPageExtension extends Extension
+{
+    private const CONTENT_CONTROLLER_ID = 'libero.content_page.controller.content.%s';
+
+    public function load(array $configs, ContainerBuilder $container) : void
+    {
+        $configuration = new Configuration();
+
+        $config = $this->processConfiguration($configuration, $configs);
+
+        foreach ($config['pages'] as $name => $page) {
+            $this->addPage($name, $page, $container);
+        }
+    }
+
+    private function addPage(string $name, array $config, ContainerBuilder $container) : void
+    {
+        $id = sprintf(self::CONTENT_CONTROLLER_ID, $name);
+        $definition = new Definition(ContentController::class);
+
+        $definition->addTag('controller.service_arguments');
+
+        $container->setDefinition($id, $definition);
+    }
+}
