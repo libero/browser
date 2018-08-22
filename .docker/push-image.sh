58055231ca2e74f72a0075167404567c865c08ff
Giorgio Sironi <g.sironi@elifesciences.org>
2018-08-22 09:03:41 +0100

    Also check TRAVIS_PULL_REQUEST

--- .docker/push-image.sh
+++ .docker/push-image.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-if [ "$TRAVIS_BRANCH" = master ]; then
-    echo $DOCKER_PASSWORD | docker login --username $DOCKER_USERNAME --password-stdin 
+if [[ "$TRAVIS_BRANCH" = "master" && "$TRAVIS_PULL_REQUEST" = "false" ]]; then
+    echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin 
 
     # tag temporarily as liberoadmin due to lack of `libero/` availability
-    docker tag libero/browser:$IMAGE_TAG liberoadmin/browser:$IMAGE_TAG
-    docker push liberoadmin/browser:$IMAGE_TAG
+    docker tag "libero/browser:$IMAGE_TAG" "liberoadmin/browser:$IMAGE_TAG"
+    docker push "liberoadmin/browser:$IMAGE_TAG"
 fi
