7c1dfa73748ab690e6d7d378183c234bf8528e1b
Chris Wilkinson <c.wilkinson@elifesciences.org>
2018-11-21 10:53:51 +0000

    Revert "Demonstrate messages"
    This reverts commit 5f1d93c165cb002f72492755d359ce5ff505b69c.

--- .travis/smoke-test.sh
+++ .travis/smoke-test.sh
@@ -11,4 +11,3 @@ trap finish EXIT
 docker-compose --file docker-compose.yaml up -d web
 docker-compose --file docker-compose.yaml exec app bin/console --version
 nc -z localhost 8080
-curl --fail http://localhost:8080/error
