186797a9c1954aaeedcbc5967c1c2cb2e5b73576
Chris Wilkinson <chriswilkinson84@gmail.com>
2018-11-22 15:27:13 +0000

    Set up Monolog (#22)
    * Set up Monolog
    
    * Demonstrate messages
    
    * Revert "Demonstrate messages"
    
    This reverts commit 5f1d93c165cb002f72492755d359ce5ff505b69c.

--- config/bundles.php
+++ config/bundles.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 return [
     Libero\ContentPageBundle\ContentPageBundle::class => ['all' => true],
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
