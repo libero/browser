4c0c281055c0ef8db04c267e209581b8e73fa9c9
Chris Wilkinson <c.wilkinson@elifesciences.org>
2018-12-06 15:09:16 +0000

    Fix vendor-extra update script

--- .travis/check-vendor-extra-updated.sh
+++ .travis/check-vendor-extra-updated.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 set -e
 
-output=$(docker-compose --file docker-compose.yaml --file docker-compose.test.yaml run app composer update libero/content-page-bundle --with-dependencies --dry-run --no-ansi)
+function finish {
+    docker-compose --file docker-compose.yaml --file docker-compose.test.yaml down --volumes
+}
+
+trap finish EXIT
+
+packages=("libero/content-page-bundle" "libero/patterns-bundle" "libero/views")
+
+output=$(docker-compose --file docker-compose.yaml --file docker-compose.test.yaml run app composer update --dry-run --no-ansi)
 echo "${output}"
 
-[[ ${output} =~ "Nothing to install or update" ]]
+for package in ${packages[@]};
+do
+    [[ ${output} != *"Updating ${package}"* ]]
+done
