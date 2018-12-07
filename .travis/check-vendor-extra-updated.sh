29ef6eb739901b4147c991f51d9ed7fe417a167d
Chris Wilkinson <c.wilkinson@elifesciences.org>
2018-12-07 13:10:24 +0000

    Improve output

--- .travis/check-vendor-extra-updated.sh
+++ .travis/check-vendor-extra-updated.sh
@@ -12,9 +12,18 @@ cd "$(dirname "$0")/.."
 while IFS=$'\n' read -r line; do packages+=("$line"); done < <(< composer.lock jq --raw-output '.packages[] | select(.dist.url | startswith("./vendor-extra/")) .name')
 
 output=$(docker-compose --file docker-compose.yaml --file docker-compose.test.yaml run app composer update --dry-run --no-ansi)
-echo "${output}"
 
+failures=0
 for package in "${packages[@]}";
 do
-    [[ ${output} != *"Updating ${package}"* ]]
+    echo "Checking ${package}"
+    if [[ ${output} == *"Updating ${package}"* ]]
+    then
+        echo "Needs to be updated"
+        ((failures++))
+    else
+        echo "Up to date"
+    fi
 done
+
+[[ ${failures} == 0 ]]
