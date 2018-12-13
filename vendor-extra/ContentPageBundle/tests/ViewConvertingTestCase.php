57e6d31feb8a27e43ef8a56a108e39d35334b7b6
Chris Wilkinson <c.wilkinson@elifesciences.org>
2018-12-13 15:16:38 +0000

    Less spacing

--- vendor-extra/ContentPageBundle/tests/ViewConvertingTestCase.php
+++ vendor-extra/ContentPageBundle/tests/ViewConvertingTestCase.php
@@ -14,18 +14,8 @@ trait ViewConvertingTestCase
     final protected function createConverter() : ViewConverter
     {
         return new CallbackViewConverter(
-            function (
-                Element $object,
-                string $template,
-                array $context
-            ) : View {
-                return new View(
-                    $template,
-                    [
-                        'element' => $object->getNodePath(),
-                        'context' => $context,
-                    ]
-                );
+            function (Element $object, string $template, array $context) : View {
+                return new View($template, ['element' => $object->getNodePath(), 'context' => $context]);
             }
         );
     }
