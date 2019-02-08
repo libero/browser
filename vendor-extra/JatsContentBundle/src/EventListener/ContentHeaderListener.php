64df1d4636b6e7a83589ee131b33b8fa52cd353f
Chris Wilkinson <c.wilkinson@elifesciences.org>
2019-02-08 10:44:55 +0000

    Replace content handlers with events

---
+++ vendor-extra/JatsContentBundle/src/EventListener/ContentHeaderListener.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Libero\JatsContentBundle\EventListener;
+
+use FluentDOM\DOM\Element;
+use Libero\ContentPageBundle\Event\CreateContentPageEvent;
+use Libero\ViewsBundle\Views\ViewConverter;
+
+final class ContentHeaderListener
+{
+    private $converter;
+
+    public function __construct(ViewConverter $converter)
+    {
+        $this->converter = $converter;
+    }
+
+    public function onCreatePage(CreateContentPageEvent $event) : void
+    {
+        $xpath = $event->getItem()->xpath();
+
+        $front = $xpath->firstOf('/libero:item/jats:article/jats:front');
+
+        if (!$front instanceof Element) {
+            return;
+        }
+
+        $title = $xpath->firstOf('jats:article-meta/jats:title-group/jats:article-title', $front);
+
+        if ($title instanceof Element) {
+            $event->setTitle((string) $title);
+        }
+
+        $event->addContent(
+            $this->converter->convert($front, '@LiberoPatterns/content-header.html.twig', $event->getContext())
+        );
+    }
+}
---
+++ vendor-extra/LiberoContentBundle/src/EventListener/ContentHeaderListener.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Libero\LiberoContentBundle\EventListener;
+
+use FluentDOM\DOM\Element;
+use Libero\ContentPageBundle\Event\CreateContentPageEvent;
+use Libero\ViewsBundle\Views\ViewConverter;
+
+final class ContentHeaderListener
+{
+    private $converter;
+
+    public function __construct(ViewConverter $converter)
+    {
+        $this->converter = $converter;
+    }
+
+    public function onCreatePage(CreateContentPageEvent $event) : void
+    {
+        $xpath = $event->getItem()->xpath();
+
+        $front = $xpath->firstOf('/libero:item/libero:front');
+
+        if (!$front instanceof Element) {
+            return;
+        }
+
+        $title = $xpath->firstOf('libero:title', $front);
+
+        if ($title instanceof Element) {
+            $event->setTitle((string) $title);
+        }
+
+        $event->addContent(
+            $this->converter->convert($front, '@LiberoPatterns/content-header.html.twig', $event->getContext())
+        );
+    }
+}
