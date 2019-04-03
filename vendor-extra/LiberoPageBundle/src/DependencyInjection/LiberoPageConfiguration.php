09b3ce1f1395eb8fb1becb3d17491f87745888e7
Chris Wilkinson <c.wilkinson@elifesciences.org>
2019-04-03 13:34:36 +0100

    Remove content page names

--- vendor-extra/LiberoPageBundle/src/DependencyInjection/LiberoPageConfiguration.php
+++ vendor-extra/LiberoPageBundle/src/DependencyInjection/LiberoPageConfiguration.php
@@ -72,12 +72,10 @@ final class LiberoPageConfiguration implements ConfigurationInterface
         /** @var ArrayNodeDefinition $pagesNode */
         $pagesNode = $builder->root('content');
         $pagesNode
+            ->normalizeKeys(false)
             ->arrayPrototype()
                 ->children()
                     ->scalarNode('path')
-                        ->isRequired()
-                    ->end()
-                    ->scalarNode('content_service')
                         ->isRequired()
                     ->end()
                 ->end()
