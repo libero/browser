942b30ef4544af7512e59345a6046c9f50b1b498
nlisgo <nathan@aeopus.com>
2019-04-05 16:21:18 +0100

    Add tests for FrontArticleTitleTeaserListener

---
+++ vendor-extra/JatsContentBundle/tests/EventListener/BuildView/FrontArticleTitleTeaserListenerTest.php
@@ -0,0 +1,170 @@
+<?php
+
+declare(strict_types=1);
+
+namespace tests\Libero\JatsContentBundle\EventListener\BuildView;
+
+use Libero\JatsContentBundle\EventListener\BuildView\FrontArticleTitleTeaserListener;
+use Libero\ViewsBundle\Event\BuildViewEvent;
+use Libero\ViewsBundle\Views\TemplateView;
+use PHPUnit\Framework\TestCase;
+use tests\Libero\LiberoPageBundle\ViewConvertingTestCase;
+use tests\Libero\LiberoPageBundle\XmlTestCase;
+
+final class FrontArticleTitleTeaserListenerTest extends TestCase
+{
+    use ViewConvertingTestCase;
+    use XmlTestCase;
+
+    /**
+     * @test
+     * @dataProvider nodeProvider
+     */
+    public function it_does_nothing_if_it_is_not_a_jats_front_element(string $xml) : void
+    {
+        $listener = new FrontArticleTitleTeaserListener($this->createFailingConverter());
+
+        $element = $this->loadElement($xml);
+
+        $event = new BuildViewEvent($element, new TemplateView('@LiberoPatterns/teaser.html.twig'));
+        $listener->onBuildView($event);
+        $view = $event->getView();
+
+        $this->assertInstanceOf(TemplateView::class, $view);
+        $this->assertSame('@LiberoPatterns/teaser.html.twig', $view->getTemplate());
+        $this->assertEmpty($view->getArguments());
+        $this->assertEmpty($view->getContext());
+    }
+
+    public function nodeProvider() : iterable
+    {
+        yield 'different namespace' => ['<front xmlns="http://example.com">foo</front>'];
+        yield 'different element' => ['<foo xmlns="http://jats.nlm.nih.gov">foo</foo>'];
+    }
+
+    /**
+     * @test
+     */
+    public function it_does_nothing_if_is_not_the_teaser_template() : void
+    {
+        $listener = new FrontArticleTitleTeaserListener($this->createFailingConverter());
+
+        $element = $this->loadElement(
+            <<<XML
+<?xml version="1.0" encoding="UTF-8"?>
+<front xmlns="http://jats.nlm.nih.gov">
+    <article-meta>
+        <title-group>
+            <article-title>foo</article-title>
+        </title-group>
+    </article-meta>
+</front>
+XML
+        );
+
+        $event = new BuildViewEvent($element, new TemplateView('template'));
+        $listener->onBuildView($event);
+        $view = $event->getView();
+
+        $this->assertInstanceOf(TemplateView::class, $view);
+        $this->assertSame('template', $view->getTemplate());
+        $this->assertEmpty($view->getArguments());
+        $this->assertEmpty($view->getContext());
+    }
+
+    /**
+     * @test
+     */
+    public function it_does_nothing_if_there_is_no_title_group() : void
+    {
+        $listener = new FrontArticleTitleTeaserListener($this->createFailingConverter());
+
+        $element = $this->loadElement('<front xmlns="http://jats.nlm.nih.gov"><article-meta/></front>');
+
+        $event = new BuildViewEvent($element, new TemplateView('@LiberoPatterns/teaser.html.twig'));
+        $listener->onBuildView($event);
+        $view = $event->getView();
+
+        $this->assertInstanceOf(TemplateView::class, $view);
+        $this->assertSame('@LiberoPatterns/teaser.html.twig', $view->getTemplate());
+        $this->assertEmpty($view->getArguments());
+        $this->assertEmpty($view->getContext());
+    }
+
+    /**
+     * @test
+     */
+    public function it_does_nothing_if_there_is_already_a_teaser_title_set() : void
+    {
+        $listener = new FrontArticleTitleTeaserListener($this->createFailingConverter());
+
+        $element = $this->loadElement(
+            <<<XML
+<?xml version="1.0" encoding="UTF-8"?>
+<front xmlns="http://jats.nlm.nih.gov">
+    <article-meta>
+        <title-group>
+            <article-title>foo</article-title>
+        </title-group>
+    </article-meta>
+</front>
+XML
+        );
+
+        $event = new BuildViewEvent(
+            $element,
+            new TemplateView('@LiberoPatterns/teaser.html.twig', ['heading' => 'bar'])
+        );
+        $listener->onBuildView($event);
+        $view = $event->getView();
+
+        $this->assertInstanceOf(TemplateView::class, $view);
+        $this->assertSame('@LiberoPatterns/teaser.html.twig', $view->getTemplate());
+        $this->assertSame(['heading' => 'bar'], $view->getArguments());
+        $this->assertEmpty($view->getContext());
+    }
+
+    /**
+     * @test
+     */
+    public function it_sets_the_text_argument() : void
+    {
+        $listener = new FrontArticleTitleTeaserListener($this->createDumpingConverter());
+
+        $element = $this->loadElement(
+            <<<XML
+<?xml version="1.0" encoding="UTF-8"?>
+<jats:front xmlns:jats="http://jats.nlm.nih.gov">
+    <jats:article-meta>
+        <jats:title-group>
+            <jats:article-title>foo</jats:article-title>
+        </jats:title-group>
+    </jats:article-meta>
+</jats:front>
+XML
+        );
+
+        $context = ['bar' => 'baz'];
+
+        $event = new BuildViewEvent(
+            $element,
+            new TemplateView('@LiberoPatterns/teaser.html.twig', [], $context)
+        );
+        $listener->onBuildView($event);
+        $view = $event->getView();
+
+        $this->assertInstanceOf(TemplateView::class, $view);
+        $this->assertSame('@LiberoPatterns/teaser.html.twig', $view->getTemplate());
+        $this->assertEquals(
+            [
+                'heading' => [
+                    'node' => '/jats:front/jats:article-meta/jats:title-group/jats:article-title',
+                    'template' => '@LiberoPatterns/heading.html.twig',
+                    'context' => ['bar' => 'baz'],
+                ],
+            ],
+            $view->getArguments()
+        );
+        $this->assertSame(['bar' => 'baz'], $view->getContext());
+    }
+}
