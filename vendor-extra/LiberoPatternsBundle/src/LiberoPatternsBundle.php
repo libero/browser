e002e2568656bc17a5050cf59ab044d60b982b87
Chris Wilkinson <c.wilkinson@elifesciences.org>
2019-05-03 08:21:49 +0100

    Add assets support to LiberoPatternsBundle

--- vendor-extra/LiberoPatternsBundle/src/LiberoPatternsBundle.php
+++ vendor-extra/LiberoPatternsBundle/src/LiberoPatternsBundle.php
@@ -4,8 +4,14 @@ declare(strict_types=1);
 
 namespace Libero\LiberoPatternsBundle;
 
+use Libero\LiberoPatternsBundle\DependencyInjection\Compiler\AssetPackagePass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 final class LiberoPatternsBundle extends Bundle
 {
+    public function build(ContainerBuilder $container) : void
+    {
+        $container->addCompilerPass(new AssetPackagePass());
+    }
 }
