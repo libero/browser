c2d7d0f0bfe4e380ea7f0c2cf8357280bcff1680
David Moulton <d.moulton@elifesciences.org>
2019-05-28 17:00:24 +0100

    Add in site header

---
+++ vendor-extra/LiberoPageBundle/src/EventListener/SiteHeaderListener.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Libero\LiberoPageBundle\EventListener;
+
+use Libero\LiberoPageBundle\Event\CreatePagePartEvent;
+use Libero\ViewsBundle\Views\ContextAwareTranslation;
+use Libero\ViewsBundle\Views\TemplateView;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+final class SiteHeaderListener
+{
+  use ContextAwareTranslation;
+  private $urlGenerator;
+
+  public function __construct(TranslatorInterface $translator, UrlGeneratorInterface $urlGenerator)
+  {
+    $this->translator = $translator;
+    $this->urlGenerator = $urlGenerator;
+  }
+
+  public function onCreatePagePart(CreatePagePartEvent $event) : void
+  {
+
+    $context = ['area' => null] + $event->getContext();
+
+    $event->addContent(
+      new TemplateView(
+        '@LiberoPatterns/site-header.html.twig',
+        ['logo' => [
+          'href' => $this->urlGenerator->generate("libero.page.homepage")
+          ]
+        ],
+        $context
+      )
+    );
+  }
+}
