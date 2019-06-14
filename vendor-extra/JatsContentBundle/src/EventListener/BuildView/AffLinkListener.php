bef25d76cca5764a4b520868570813ab97bf7fec
Chris Wilkinson <c.wilkinson@elifesciences.org>
2019-06-14 09:51:57 +0100

    Avoid HTML in an affiliation

--- vendor-extra/JatsContentBundle/src/EventListener/BuildView/AffLinkListener.php
+++ vendor-extra/JatsContentBundle/src/EventListener/BuildView/AffLinkListener.php
@@ -5,27 +5,18 @@ declare(strict_types=1);
 namespace Libero\JatsContentBundle\EventListener\BuildView;
 
 use FluentDOM\DOM\Element;
-use Libero\ViewsBundle\Views\ConvertsChildren;
 use Libero\ViewsBundle\Views\TemplateView;
 use Libero\ViewsBundle\Views\View;
 use Libero\ViewsBundle\Views\ViewBuildingListener;
-use Libero\ViewsBundle\Views\ViewConverter;
 use function Libero\ViewsBundle\array_has_key;
-use function Libero\ViewsBundle\string_is;
 
-final class LinkListener
+final class AffLinkListener
 {
-    use ConvertsChildren;
     use ViewBuildingListener;
-
-    public function __construct(ViewConverter $converter)
-    {
-        $this->converter = $converter;
-    }
 
     protected function handle(Element $object, TemplateView $view) : View
     {
-        return $view->withArgument('text', $this->convertChildren($object, $view->getContext()));
+        return $view->withArgument('text', $object->textContent);
     }
 
     protected function template() : string
@@ -35,7 +26,7 @@ final class LinkListener
 
     protected function canHandleElement(Element $element) : bool
     {
-        return string_is($element->clarkNotation(), '{http://jats.nlm.nih.gov}kwd', '{http://jats.nlm.nih.gov}subject');
+        return '{http://jats.nlm.nih.gov}aff' === $element->clarkNotation();
     }
 
     protected function canHandleArguments(array $arguments) : bool
