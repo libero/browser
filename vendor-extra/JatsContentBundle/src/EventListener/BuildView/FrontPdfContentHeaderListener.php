66dbe387bfefa2494f3fb13ecc3bd95a17687fb1
Chris Wilkinson <c.wilkinson@elifesciences.org>
2019-06-14 08:29:04 +0100

    Add article PDF

---
+++ vendor-extra/JatsContentBundle/src/EventListener/BuildView/FrontPdfContentHeaderListener.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Libero\JatsContentBundle\EventListener\BuildView;
+
+use FluentDOM\DOM\Element;
+use GuzzleHttp\Psr7\UriResolver;
+use Libero\ViewsBundle\Views\TemplateView;
+use Libero\ViewsBundle\Views\View;
+use Libero\ViewsBundle\Views\ViewBuildingListener;
+use function GuzzleHttp\Psr7\uri_for;
+use function Libero\ViewsBundle\array_has_key;
+
+final class FrontPdfContentHeaderListener
+{
+    use ViewBuildingListener;
+
+    protected function handle(Element $object, TemplateView $view) : View
+    {
+        $pdf = $object->ownerDocument->xpath()
+            ->firstOf("jats:article-meta/jats:self-uri[@content-type='pdf']", $object);
+
+        if (!$pdf instanceof Element) {
+            return $view;
+        }
+
+        $uri = UriResolver::resolve(
+            uri_for($pdf->baseURI),
+            uri_for($pdf->getAttributeNS('http://www.w3.org/1999/xlink', 'href'))
+        );
+
+        return $view->withArgument('downloadIconLink', ['attributes' => ['href' => (string) $uri]]);
+    }
+
+    protected function template() : string
+    {
+        return '@LiberoPatterns/content-header.html.twig';
+    }
+
+    protected function canHandleElement(Element $element) : bool
+    {
+        return '{http://jats.nlm.nih.gov}front' === $element->clarkNotation();
+    }
+
+    protected function canHandleArguments(array $arguments) : bool
+    {
+        return !array_has_key($arguments, 'downloadIconLink');
+    }
+}
