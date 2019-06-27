aa81033ffceda60efa1f4cf75729387cf26614f8
Chris Wilkinson <c.wilkinson@elifesciences.org>
2019-06-27 12:26:36 +0100

    Inject packages

--- vendor-extra/LiberoPageBundle/src/EventListener/InfoBarListener.php
+++ vendor-extra/LiberoPageBundle/src/EventListener/InfoBarListener.php
@@ -7,8 +7,7 @@ namespace Libero\LiberoPageBundle\EventListener;
 use Libero\LiberoPageBundle\Event\CreatePagePartEvent;
 use Libero\ViewsBundle\Views\ContextAwareTranslation;
 use Libero\ViewsBundle\Views\TemplateView;
-use Symfony\Component\Asset\PathPackage;
-use Symfony\Component\Asset\VersionStrategy\EmptyVersionStrategy;
+use Symfony\Component\Asset\Packages;
 use Symfony\Contracts\Translation\TranslatorInterface;
 use const Libero\LiberoPatternsBundle\MAIN_GRID_FULL;
 
@@ -18,26 +17,27 @@ final class InfoBarListener
     public const ATTENTION = ['name' => 'attention', 'imagePath' => 'attention.svg'];
     public const INFO = ['name' => 'info', 'imagePath' => 'info.svg'];
     public const SUCCESS = ['name' => 'success', 'imagePath' => 'success.svg'];
-    public const WARNING = ['name' => 'warning', 'imagePath' => ''];
+    public const WARNING = ['name' => 'warning'];
 
     private $urlGenerator;
+    private $packages;
 
-    public function __construct(TranslatorInterface $translator)
+    public function __construct(TranslatorInterface $translator, Packages $packages)
     {
         $this->translator = $translator;
+        $this->packages = $packages;
     }
 
     public function onCreatePagePart(CreatePagePartEvent $event) : void
     {
         $context = ['area' => MAIN_GRID_FULL] + $event->getContext();
-        $package = new PathPackage('/images/info-bar', new EmptyVersionStrategy());
 
         $event->addContent(
             new TemplateView(
                 '@LiberoPatterns/info-bar.html.twig',
                 [   'type' => self::INFO['name'],
                     'image' => [
-                        'src' => $package->getUrl(self::INFO['imagePath']),
+                        'src' => $this->packages->getUrl('images/info-bar/'.self::INFO['imagePath'], 'libero_patterns'),
                     ],
                     'content' => [
                       'text' => $this->translate('libero.page.infobar.demo.text', $context),
