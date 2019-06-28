17b85fd1a36b5195502be8737a768841cefb907e
Chris Wilkinson <c.wilkinson@elifesciences.org>
2019-06-28 12:50:13 +0100

    Add teaser details

---
+++ vendor-extra/JatsContentBundle/src/EventListener/BuildView/FrontSubjectGroupTeaserListener.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Libero\JatsContentBundle\EventListener\BuildView;
+
+use DOMNodeList;
+use FluentDOM\DOM\Element;
+use Libero\ViewsBundle\Views\ContextAwareTranslation;
+use Libero\ViewsBundle\Views\ConvertsLists;
+use Libero\ViewsBundle\Views\TemplateView;
+use Libero\ViewsBundle\Views\View;
+use Libero\ViewsBundle\Views\ViewBuildingListener;
+use Libero\ViewsBundle\Views\ViewConverter;
+use Symfony\Contracts\Translation\TranslatorInterface;
+use function array_map;
+use function count;
+use function Libero\ViewsBundle\array_has_key;
+
+final class FrontSubjectGroupTeaserListener
+{
+    use ContextAwareTranslation;
+    use ConvertsLists;
+    use ViewBuildingListener;
+
+    public function __construct(ViewConverter $converter, TranslatorInterface $translator)
+    {
+        $this->converter = $converter;
+        $this->translator = $translator;
+    }
+
+    protected function handle(Element $object, TemplateView $view) : View
+    {
+        /** @var DOMNodeList<Element> $subjects */
+        $subjects = $object->ownerDocument->xpath()->evaluate(
+            'jats:article-meta/jats:article-categories/jats:subj-group[@subj-group-type="heading"]/jats:subject',
+            $object
+        );
+
+        if (0 === count($subjects)) {
+            return $view;
+        }
+
+        return $view->withArgument(
+            'categories',
+            [
+                'attributes' => [
+                    'aria-label' => $this->translate(
+                        'libero.patterns.teaser.categories.label',
+                        $view->getContext()
+                    ),
+                ],
+                'items' => array_map(
+                    static function (TemplateView $link) : array {
+                        return ['content' => $link->getArguments()];
+                    },
+                    $this->convertList($subjects, '@LiberoPatterns/link.html.twig', $view->getContext())
+                ),
+            ]
+        );
+    }
+
+    protected function template() : string
+    {
+        return '@LiberoPatterns/teaser.html.twig';
+    }
+
+    protected function canHandleElement(Element $element) : bool
+    {
+        return '{http://jats.nlm.nih.gov}front' === $element->clarkNotation();
+    }
+
+    protected function canHandleArguments(array $arguments) : bool
+    {
+        return !array_has_key($arguments, 'categories');
+    }
+}
---
+++ vendor-extra/JatsContentBundle/src/EventListener/BuildView/FrontTeaserMetaListener.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Libero\JatsContentBundle\EventListener\BuildView;
+
+use FluentDOM\DOM\Element;
+use Libero\ViewsBundle\Views\ContextAwareTranslation;
+use Libero\ViewsBundle\Views\TemplateView;
+use Libero\ViewsBundle\Views\View;
+use Libero\ViewsBundle\Views\ViewBuildingListener;
+use Libero\ViewsBundle\Views\ViewConverter;
+use Symfony\Contracts\Translation\TranslatorInterface;
+use function Libero\ViewsBundle\array_has_key;
+
+final class FrontTeaserMetaListener
+{
+    use ContextAwareTranslation;
+    use ViewBuildingListener;
+
+    private $converter;
+
+    public function __construct(ViewConverter $converter, TranslatorInterface $translator)
+    {
+        $this->converter = $converter;
+        $this->translator = $translator;
+    }
+
+    protected function handle(Element $object, TemplateView $view) : View
+    {
+        $meta = $this->converter->convert($object, '@LiberoPatterns/content-meta.html.twig', $view->getContext());
+
+        if (!$meta instanceof TemplateView) {
+            return $view->withArgument('meta', $meta);
+        }
+
+        $meta = $meta->getArguments();
+
+        if (empty($meta)) {
+            return $view;
+        }
+
+        if (!isset($meta['attributes']['aria-label'])) {
+            $meta['attributes']['aria-label'] = $this->translate(
+                'libero.patterns.teaser.meta.label',
+                $view->getContext()
+            );
+        }
+
+        return $view->withArgument('meta', $meta);
+    }
+
+    protected function template() : string
+    {
+        return '@LiberoPatterns/teaser.html.twig';
+    }
+
+    protected function canHandleElement(Element $element) : bool
+    {
+        return '{http://jats.nlm.nih.gov}front' === $element->clarkNotation();
+    }
+
+    protected function canHandleArguments(array $arguments) : bool
+    {
+        return !array_has_key($arguments, 'meta');
+    }
+}
