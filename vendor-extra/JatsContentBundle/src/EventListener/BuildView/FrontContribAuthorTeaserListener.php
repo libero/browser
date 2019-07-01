4922d8461752b5feee7968c00762320c2de1da16
Chris Wilkinson <c.wilkinson@elifesciences.org>
2019-07-01 10:59:46 +0100

    Give access to any author

--- vendor-extra/JatsContentBundle/src/EventListener/BuildView/FrontContribAuthorTeaserListener.php
+++ vendor-extra/JatsContentBundle/src/EventListener/BuildView/FrontContribAuthorTeaserListener.php
@@ -13,16 +13,12 @@ use Libero\ViewsBundle\Views\View;
 use Libero\ViewsBundle\Views\ViewBuildingListener;
 use Libero\ViewsBundle\Views\ViewConverter;
 use Symfony\Contracts\Translation\TranslatorInterface;
-use function array_combine;
-use function array_fill_keys;
 use function array_keys;
-use function array_reduce;
-use function array_replace;
-use function array_slice;
+use function array_map;
 use function count;
-use function iterator_to_array;
 use function Libero\ViewsBundle\array_has_key;
 use function preg_split;
+use function substr;
 use const PREG_SPLIT_DELIM_CAPTURE;
 use const PREG_SPLIT_NO_EMPTY;
 
@@ -47,17 +43,6 @@ final class FrontContribAuthorTeaserListener
             return $view;
         }
 
-        $contribs = $this->convertList(
-            array_slice(iterator_to_array($contribs), 0, 3),
-            '@LiberoPatterns/link.html.twig',
-            $view->getContext()
-        );
-
-        $contribs = array_combine(
-            array_slice(['{author1}', '{author2}', '{author3}'], 0, count($contribs)),
-            $contribs
-        ) ?: [];
-
         $text = $this->translate(
             'libero.patterns.teaser.authors',
             $view->getContext(),
@@ -65,20 +50,31 @@ final class FrontContribAuthorTeaserListener
         );
 
         /** @var array<string> $replacements */
-        $replacements = preg_split('/({author(?:1|2|3)})/', $text, -1, PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_NO_EMPTY);
+        $replacements = preg_split(
+            '/{author([1-9][0-9]*)}/',
+            " {$text}",
+            -1,
+            PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_NO_EMPTY
+        );
+
+        $replacements[0] = substr($replacements[0], 1);
 
         $details = [
-            'text' => array_replace(
-                $replacements,
-                ...array_reduce(
-                    array_keys($contribs),
-                    static function (array $carry, string $key) use ($replacements, $contribs) : array {
-                        $carry[] = array_fill_keys(array_keys($replacements, $key, true), $contribs[$key]);
+            'text' => array_map(
+                function (int $key) use ($contribs, $replacements, $view) {
+                    if (~$key & 1) {
+                        return $replacements[$key];
+                    }
 
-                        return $carry;
-                    },
-                    []
-                )
+                    $contrib = $contribs->item((int) $replacements[$key] - 1);
+
+                    if (!$contrib instanceof Element) {
+                        return "{author{$replacements[$key]}}";
+                    }
+
+                    return $this->converter->convert($contrib, '@LiberoPatterns/link.html.twig', $view->getContext());
+                },
+                array_keys($replacements)
             ),
         ];
 
