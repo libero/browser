47d2abb63745917c1e2827ae138570cbf3a72988
Chris Wilkinson <c.wilkinson@elifesciences.org>
2019-07-05 13:16:55 +0100

    Remove debug.event_dispatcher (#96)

--- vendor-extra/ViewsBundle/src/ViewsBundle.php
+++ vendor-extra/ViewsBundle/src/ViewsBundle.php
@@ -4,8 +4,14 @@ declare(strict_types=1);
 
 namespace Libero\ViewsBundle;
 
+use Libero\ViewsBundle\DependencyInjection\Compiler\RemoveDebugEventDispatcherPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 final class ViewsBundle extends Bundle
 {
+    public function build(ContainerBuilder $container) : void
+    {
+        $container->addCompilerPass(new RemoveDebugEventDispatcherPass());
+    }
 }
